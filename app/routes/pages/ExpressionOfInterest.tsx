66fb1e63bc2d8a7d17e8c099d63e571458d5a20a
Tom <tom_sinnott@hotmail.com>
2024-02-17 19:26:21 +0800

    initial fluent add

--- app/routes/pages/ExpressionOfInterest.tsx
+++ app/routes/pages/ExpressionOfInterest.tsx
@@ -1,12 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { makeStyles } from '@fluentui/react-components';
 import * as React from 'react';
+
+const useStyles = makeStyles({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '80%'
+    }
+});
 
 
 const ExpressOfInterest = () => {
+    const classes = useStyles();
 
+    return (<div className={classes.container}>
+        <h2>Expression of Interest</h2>
 
-
-
-    return <div>Foo</div>
+    </div>)
 }
 
 export default ExpressOfInterest;
